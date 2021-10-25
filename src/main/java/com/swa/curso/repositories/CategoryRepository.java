69c59a004c31583fdb7e26655472b2401fc395ce
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-25 16:57:11 -0300

    Adicionadas funções de categoria

---
+++ src/main/java/com/swa/curso/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.swa.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.swa.curso.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
