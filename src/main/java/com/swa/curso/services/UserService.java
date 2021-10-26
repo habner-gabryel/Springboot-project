208540e4d28e14ec3ed5078c00ca51d83a230279
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-26 11:28:39 -0300

    Tratamento de exception database em delete

--- src/main/java/com/swa/curso/services/UserService.java
+++ src/main/java/com/swa/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.swa.curso.entities.User;
 import com.swa.curso.repositories.UserRepository;
+import com.swa.curso.services.exceptions.DataBaseException;
 import com.swa.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);			
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User att) {
