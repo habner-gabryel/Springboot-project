c944928154c7dca01bef23c5b99f1eda8e7a262e
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-26 10:23:16 -0300

    Adicionado payment com relação ont-to-one.

--- src/main/java/com/swa/curso/config/TestConfig.java
+++ src/main/java/com/swa/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.swa.curso.entities.Category;
 import com.swa.curso.entities.Order;
 import com.swa.curso.entities.OrderItem;
+import com.swa.curso.entities.Payment;
 import com.swa.curso.entities.Product;
 import com.swa.curso.entities.User;
 import com.swa.curso.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2019-07-21T06:42:10Z"), o2);
+		
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
